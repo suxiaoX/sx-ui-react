cdcf76ff1b33fd8488f800e77b27b6f1b6a1b266
suxiao <suxiao@mixiaojin.com>
2022-09-07 18:02:09 +0800

    feat: 测试npm发包形式

---
+++ packages/tag/es/index.d.ts
@@ -0,0 +1,6 @@
+import type { FC } from 'react';
+import type { TagProps as AntdTagProps } from 'antd';
+import './index.less';
+declare type AlertProps = AntdTagProps;
+declare const Tag: FC<AlertProps>;
+export default Tag;
